becf37f373ee90cbdfeb330e0fe3bcc2107f979d
steambread666 <15927042163@163.com>
2020-05-02 09:18:46 +0800

    100

---
+++ 00-每日一题/20200502_100.cpp
@@ -0,0 +1,13 @@
+//
+// Created by zanbo on 2020/5/2.
+//
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if( (p==NULL && q!=NULL) || (p!=NULL && q==NULL)) return false;
+        if(p==NULL && q==NULL) return true;
+        if(p->val != q->val) return false;
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+    }
+};
